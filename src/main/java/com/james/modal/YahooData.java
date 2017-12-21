20bbada336424f695bfe5bd555b2a0339f0e0473
James Qin <qinchuan77@gmail.com>
2017-12-21 13:21:11 -0500

    fix bugs

--- src/main/java/com/james/modal/YahooData.java
+++ src/main/java/com/james/modal/YahooData.java
@@ -15,24 +15,24 @@ public class YahooData {
 		
 		output += this.ticker + "\n";
 		
-		output +=" \t" + "statistics";
-		output +=" \t" + "\t" + Statistics.getLable();
-		output +=" \t" + "\t" + statistics;
+		output +=" \t" + "statistics" + "\n";
+		output +=" \t" + "\t" + Statistics.getLable() + "\n";
+		output +=" \t" + "\t" + statistics + "\n";
 		
-		output +=" \t" + "major holder";
-		output +=" \t" + "\t" + MajorHolder.getLable();
-		output +=" \t" + "\t" + majorHolder;
+		output +=" \t" + "major holder" + "\n";
+		output +=" \t" + "\t" + MajorHolder.getLable() + "\n";
+		output +=" \t" + "\t" + majorHolder + "\n";
 		
 		if (institutionalHolders.size() > 0) {
-			output +=" \t" + "institutional holder";
-			output +=" \t" + "\t" + TopInstitutionalHolder.getLable();
-			for (TopInstitutionalHolder institutionalHolder : institutionalHolders) output +=" \t" + "\t" + institutionalHolder;
+			output +=" \t" + "institutional holder" + "\n";
+			output +=" \t" + "\t" + TopInstitutionalHolder.getLable() + "\n";
+			for (TopInstitutionalHolder institutionalHolder : institutionalHolders) output +=" \t" + "\t" + institutionalHolder + "\n";
 		}
 		
 		if (mutualFundHolders.size() > 0) {
-			output +=" \t" + "mutual fund holder";
-			output +=" \t" + "\t" + TopMutualFundHolder.getLable();
-			for (TopMutualFundHolder mutualFundHolder : mutualFundHolders) output +=" \t" + "\t" + mutualFundHolder;
+			output +=" \t" + "mutual fund holder" + "\n";
+			output +=" \t" + "\t" + TopMutualFundHolder.getLable() + "\n";
+			for (TopMutualFundHolder mutualFundHolder : mutualFundHolders) output +=" \t" + "\t" + mutualFundHolder + "\n";
 		}
 		
 		return output;
